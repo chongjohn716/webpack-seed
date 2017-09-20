7afb5773bb88a9ca0b1dff2c93e98f6c3b985256
chongjohn716 <chong.john716@gmail.com>
2017-09-21 01:19:19 +0800

    add plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,52 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const library = '[name]_lib'
+let path = require('path')
+let HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: {
-    vendors: ['lodash']
+    app: path.resolve(__dirname, 'src', 'main.js')
   },
 
   output: {
-    filename: '[name].dll.js',
-    path: path.resolve(__dirname, 'dist'),
-    library
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+
+  externals: {
+    jquery: 'window.jQuery',
+    $fadeIn: 'window.$fadeIn',
+    log: 'window.log'
+  },
+
+  devtool: 'eval-source-map',
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        include: path.resolve(__dirname, 'src')
+      },
+      {
+        test: /\.css$/,
+        loader: ['style-loader', 'css-loader'],
+        include: path.resolve(__dirname)
+      },
+      {
+        test: /\.scss$/,
+        loader: ['style-loader', 'css-loader', 'sass-loader'],
+        include: path.resolve(__dirname, 'src')
+      }
+    ]
   },
 
   plugins: [
-    new webpack.DllPlugin({
-      path: path.resolve(__dirname, 'dist', '[name]-manifest.json'),
-      // This must match the output.library option above
-      name: library
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'src', 'index.html')
+    }),
+
+    new webpack.DllReferencePlugin({
+      context: __dirname,
+      manifest: require('./dist/vendors-manifest.json')
     })
   ]
 };
--- webpack.dll.config.js
+++ webpack.dll.config.js
@@ -1,22 +1,60 @@
 const path = require('path')
 const webpack = require('webpack')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const library = '[name]_lib'
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const library = '[name].[hash]'
 
 module.exports = {
   entry: {
-    vendors: ['lodash']
+    vendors: ['lodash'],
+    "vendors.css": ['minireset.css']
   },
 
   output: {
-    filename: '[name].dll.js',
+    filename: '[name].[hash].js',
     path: path.resolve(__dirname, 'dist'),
     library
   },
 
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        loader: ['style-loader', 'css-loader'],
+        include: path.resolve(__dirname)
+      }
+    ]
+  },
+
   plugins: [
+    // 删除文件
+    new CleanWebpackPlugin(
+      //匹配删除的文件
+      [
+        'dist/manifest.*.json',
+        // TODO: 分离 CSS
+        // 'dist/vendor.*.css',
+        'dist/vendors.*.js'
+      ],
+      {
+        root: __dirname,       　　　  　　　//根目录
+        verbose: true,        　　　　　　　 //开启在控制台输出信息
+        dry: false        　　　　　　　　　 //启用删除文件
+      }
+    ),
+
+    // 代码 JS 压缩
+    new webpack.optimize.UglifyJsPlugin({
+      warnings: false,
+      compress: {
+        join_vars: true,
+        warnings: false,
+      },
+      toplevel: false,
+      ie8: false
+    }),
+
     new webpack.DllPlugin({
-      path: path.resolve(__dirname, 'dist', '[name]-manifest.json'),
+      path: path.resolve(__dirname, 'dist', 'manifest.[hash].json'),
       // This must match the output.library option above
       name: library
     })
